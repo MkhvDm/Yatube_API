550f079340c4f84a645f5051188412b02269fffa
Dmitriy Mikheev <mkhvdm@gmail.com>
2022-07-25 10:35:34 +0300

    start_proj

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import PostViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='posts')
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,18 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+
+from posts.models import Post
+# from .permissions import IsAuthor
+
+from .serializers import CommentSerializer, PostSerializer
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    # permission_classes = (AllowAny, )
+    permission_classes = (IsAuthenticated, )
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
